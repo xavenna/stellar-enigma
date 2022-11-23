6e5802c80e911e6a9d457ce84b3a2dbb1e5f81c9
xavenna <adam.evanson@gmail.com>
2022-11-23 00:39:52 -0500

    Fixed diagonal movement bug for pushable objects

--- src/callback.cpp
+++ src/callback.cpp
@@ -1,4 +1,5 @@
 #include "callback.h"
+#include <cmath>
 
 // This file contains functions for handling interactions
 // between player and entities/objects
@@ -40,16 +41,16 @@ void solidBehave(Object& ob, MapData* md) {
   bool xInt = xAfter && !xBefore && ((yAfter && yBefore) || (!yBefore && yAfter));
   bool yInt = yAfter && !yBefore && ((xAfter && xBefore) || (!xBefore && xAfter));
 
-  if(xInt && pmin.x <= omin.x) {
+  if(xInt && pmin.x < omin.x) {
     md->player.setXPos(ob.getXPos()-md->player.getSize().y);
   }
-  if(yInt && pmin.y <= omin.y) {
+  if(yInt && pmin.y < omin.y) {
     md->player.setYPos(ob.getYPos()-md->player.getSize().x);
   }
-  if(xInt && pmin.x >= omin.x) {
+  if(xInt && pmin.x > omin.x) {
     md->player.setXPos(ob.getXPos()+ob.getSize().x);
   }
-  if(yInt && pmin.y >= omin.y) {
+  if(yInt && pmin.y > omin.y) {
     md->player.setYPos(ob.getYPos()+ob.getSize().y);
   }
 }
@@ -58,39 +59,88 @@ void solidBehave(Object& ob, MapData* md) {
 void pushBehave(Object& ob, MapData* md) {
   //crate: attempts to move md->playerSpeed units in md->player facing direction
   
-  int residSpeed;
-  switch(md->player.getFacing()) {
-  case Right:
-    residSpeed = md->player.getXPos()+md->player.getSize().x-ob.getXPos();
-    break;
-  case Down:
-    residSpeed = md->player.getYPos()+md->player.getSize().y-ob.getYPos();
-    break;
-  case Left:
-    residSpeed = ob.getXPos()+ob.getSize().x-md->player.getXPos();
-    break;
-  case Up:
-    residSpeed = ob.getYPos()+ob.getSize().y-md->player.getYPos();
-    break;
+  sf::Vector2i pmin{md->player.getPos()};
+  sf::Vector2i pmax{pmin+md->player.getSize()-sf::Vector2i(1,1)};
+  sf::Vector2i plmin{md->player.getLastPos()};
+  sf::Vector2i plmax{plmin+md->player.getSize()-sf::Vector2i(1,1)};
+
+  //just incase objects move, so the algorithm doesn't break
+  sf::Vector2i omin{ob.getPos()};
+  sf::Vector2i omax{omin+ob.getSize()-sf::Vector2i(1,1)};
+  sf::Vector2i olmin{ob.getLastPos()};
+  sf::Vector2i olmax{olmin+ob.getSize()-sf::Vector2i(1,1)};
+  //calculate delta-r
+  sf::Vector2i deltaR = pmin - plmin;
+  //determine interaction for each direction:
+  //was player intersecting with object on the x-axis?
+  bool xAfter = !(omax.x < pmin.x || omin.x > pmax.x);
+  bool xBefore = !(olmax.x < plmin.x || olmin.x > plmax.x);
+
+  bool yAfter = !(omax.y < pmin.y || omin.y > pmax.y);
+  bool yBefore = !(olmax.y < plmin.y || olmin.y > plmax.y);
+
+  //x, y interactions
+  bool xInt = xAfter && !xBefore && ((yAfter && yBefore) || (!yBefore && yAfter));
+  bool yInt = yAfter && !yBefore && ((xAfter && xBefore) || (!xBefore && xAfter));
+
+  if(xInt && yInt) {
+    return; //For now, diagonal
   }
-  int moveDistance = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed, md->player.getFacing());
+
+  int px = 0;
+  int py = 0;
+  sf::Vector2i residSpeed;
+  if(xInt && pmin.x < omin.x) {
+    residSpeed.x = md->player.getXPos()+md->player.getSize().x-ob.getXPos();
+    px = 1;
+  }  //moving right
+  if(yInt && pmin.y < omin.y) {
+    residSpeed.y = md->player.getYPos()+md->player.getSize().y-ob.getYPos();
+    py = 1;
+  }
+  if(xInt && pmin.x > omin.x) {
+    residSpeed.x = ob.getXPos()+ob.getSize().x-md->player.getXPos();
+    px = -1;
+  }
+  if(yInt && pmin.y > omin.y) {
+    residSpeed.y = ob.getYPos()+ob.getSize().y-md->player.getYPos();
+    py = -1;
+  }
+  sf::Vector2i moveDistance;
+  if(px == 1) {
+    moveDistance.x = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed.x, Right);
+  }
+  else if(px == -1) {
+    moveDistance.x = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed.x, Left);
+  }
+  else {
+    moveDistance.x = 0;
+  }
+  if(py == 1) {
+    moveDistance.y = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed.y, Down);
+  }
+  else if(py == -1) {
+    moveDistance.y = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed.y, Up);
+  }
+  else {
+    moveDistance.y = 0;
+  }
+  moveDistance.y = md->levelSlot.validMove(ob.getPos(), ob.getSize(), residSpeed.y, md->player.getFacing());
   //move the object moveDistance Units in md->player.getFacing() direction
-  switch(md->player.getFacing()) {
-  case Right:
-    ob.setXPos(ob.getXPos()+moveDistance);
-    md->player.setXPos(md->player.getXPos()-(residSpeed-moveDistance));
-    break;
-  case Down:
-    ob.setYPos(ob.getYPos()+moveDistance);
-    md->player.setYPos(md->player.getYPos()-(residSpeed-moveDistance));
-    break;
-  case Left:
-    ob.setXPos(ob.getXPos()-moveDistance);
-    md->player.setXPos(md->player.getXPos()+(residSpeed-moveDistance));
-    break;
-  case Up:
-    ob.setYPos(ob.getYPos()-moveDistance);
-    md->player.setYPos(md->player.getYPos()+(residSpeed-moveDistance));
-    break;
+  if(xInt && pmin.x < omin.x) {
+    ob.setXPos(ob.getXPos()+moveDistance.x);
+    md->player.setXPos(md->player.getXPos()-(residSpeed.x-moveDistance.x));
+  }
+  if(yInt && pmin.y < omin.y) {
+    ob.setYPos(ob.getYPos()+moveDistance.y);
+    md->player.setYPos(md->player.getYPos()-(residSpeed.y-moveDistance.y));
+  }
+  if(xInt && pmin.x > omin.x) {
+    ob.setXPos(ob.getXPos()-moveDistance.x);
+    md->player.setXPos(md->player.getXPos()+(residSpeed.x-moveDistance.x));
+  }
+  if(yInt && pmin.y > omin.y) {
+    ob.setYPos(ob.getYPos()-moveDistance.y);
+    md->player.setYPos(md->player.getYPos()+(residSpeed.y-moveDistance.y));
   }
 }
